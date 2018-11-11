57a890946eff3ec8ecfe841bf7e7444691ab6fa4
npettyjohn <nicholas.pettyjohn@gmail.com>
2018-11-11 12:14:54 -0800

    Another attempt at adding meta tag to head section of homepage, so it can be indexed by Google.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(private meta: Meta) {
+    this.meta.addTag({ name: 'google-site-verification', content: 'Ep--HoNFPIjXzGyD-wQK-JtQ1qqyJINjyusv9RqtIMk' });
+  }
 }
